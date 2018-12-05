7f7af9a27d7e9979edb0f7998b972107a409fb04
Brandon Surmanski <b.surmanski@gmail.com>
2018-12-04 22:09:14 -0500

    create SDL2 jumpy example

---
+++ gravity_test/README.md
@@ -0,0 +1,13 @@
+# Gravity Test
+A simple example showing gravity.
+
+## Building
+This requires GCC and Make to build, and uses SDL2 as a library.
+
+GCC and Make should already be install on Linux and OSX.
+
+SDL2 can be installed from [The SDL site](https://www.libsdl.org/download-2.0.php).
+Instructions are included, basically copy the SDL2.framework file to /Library/Frameworks
+
+To build, run `make osx` on the command line.
+To run, run `./jumpy`.
--- gravity_test/main.c
+++ gravity_test/main.c
@@ -6,17 +6,21 @@
  */
 
 #include <string.h>
+#include <stdbool.h>
 
 // SDL is a library we use for creating a
 // window and handle key events.
-#include <SDL/SDL.h>
+// All functions that start with SDL_ are
+// from this library.
+#include <SDL2/SDL.h>
 
 // Define a 'struct' to hold Player variables.
 typedef struct Player {
-    float x;
-    float y;
-    float vx;
-    float vy;
+  float x;
+  float y;
+  float vx;
+  float vy;
+  bool on_floor;
 } Player;
 
 // Global variables.
@@ -24,163 +28,177 @@ typedef struct Player {
 // but we'll make an exception for these.
 static int running = 1;
 static int paused = 1;
-static SDL_Surface *screen;
 
 // Create and initialize a new player.
 Player create_player() {
-    Player player;
-    player.x = 100;
-    player.y = 100;
-    player.vx = 1.0f;
-    player.vy = -2.0f;
-    return player;
-}
-
-// check if a given x,y is a valid surface coordinate.
-int inSurface(SDL_Surface *s, int x, int y)
-{
-    return (x > 0 && x < s->w && y > 0 && y < s->h);
+  Player player;
+  player.x = 100;
+  player.y = 100;
+  player.vx = 1.0f;
+  player.vy = -2.0f;
+  player.on_floor = false;
+  return player;
 }
 
 // Set the RGBA value of a pixel at x,y.
-void setPixel(SDL_Surface *s, int x, int y, int color)
-{
-    if(inSurface(s,x,y)){
-        memcpy(
-                s->pixels + x * s->format->BytesPerPixel + y * s->pitch,
-                &color,
-                s->format->BytesPerPixel
-                );
-    }
+void setPixel(int *pixels, int pitch, int x, int y, int color) {
+    pixels[x + y * pitch] = color;
 }
 
 // Get the RGBA value of a pixel at x,y.
-int getPixel(SDL_Surface *s, int x, int y)
-{
-    int ret = 0;
-    if(inSurface(s,x,y)){
-        memcpy(&ret,
-                s->pixels + x * s->format->BytesPerPixel + y * s->pitch,
-                s->format->BytesPerPixel);
-    }
-    return ret;
+int getPixel(int *pixels, int pitch, int x, int y) {
+  return pixels[x + y * pitch];
 }
 
-void update(Player *player)
-{
-    // Handle closing the window.
-    SDL_Event e;
-    while(SDL_PollEvent(&e)) {
-        if(e.type == SDL_QUIT)
-            running = 0;
+void handle_input(Player *player) {
+  // Handle closing the window.
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) running = 0;
+  }
+
+  // Get the current keyboard state.
+  SDL_PumpEvents();
+  const uint8_t *keystate = SDL_GetKeyboardState(0);
+
+  // control
+  if (keystate[SDL_SCANCODE_RIGHT]) {
+    player->vx += 0.2;
+  }
+  if (keystate[SDL_SCANCODE_LEFT]) {
+    player->vx -= 0.2;
+  }
+
+  // handle jump
+  if (keystate[SDL_SCANCODE_SPACE] &&
+      player->on_floor) {
+    player->vy = -2;
+  }
+}
+
+void update(Player *player) {
+  // constant wall y-coordinate
+  const int wally = 120;
+
+  // if the player is below the floor.
+  // (y gets bigger going down)
+  if (player->y >= wally) {
+    player->y = wally;
+    player->vy *= -0.5;
+    player->vx *= 0.97;
+    player->on_floor = true;
+
+  } else {
+    // apply gravity by speeding up the player downward.
+      player->on_floor = false;
+    player->vy += 0.1;
+  }
+
+  // bounce off left wall.
+  if (player->x <= 0) {
+    player->x = 1;
+    player->vx = -player->vx;
+  }
+
+  // bounce off right wall.
+  if (player->x >= 320) {
+    player->x = 319;
+    player->vx = -player->vx;
+  }
+
+  // Apply velocity each timestep.
+  player->x += player->vx;
+  player->y += player->vy;
+}
+
+void draw(SDL_Texture *texture, Player *player) {
+  int w = 320;
+  int h = 240;
+
+  int *pixels;
+  int pitch;
+  SDL_LockTexture(texture, NULL, (void**) &pixels, &pitch);
+  pitch /= 4; // make pitch relative to int instead of byte.
+
+  // wacky stuff to draw player trail.
+  // Fade the whole screen over time.
+  int i, j;
+  for (i = 0; i < h; i++) {
+    for (j = 0; j < w; j++) {
+      int p = getPixel(pixels, pitch, j, i);
+      // set topmost bit to zero.
+      p &= 0x7fffffff;
+      // shift all bits in pixel to the right.
+      // This has weird color effects because
+      // it treats the RGBA bytes as a single (int)
+      // value and pushes the bits between them.
+      // Since the left most bit is zero, eventually
+      // all the bits are push off the end (over
+      // the course of ~32 frames) and the
+      // pixel will be black.
+      p >>= 1;
+      setPixel(pixels, pitch, j, i, p);
     }
+  }
 
-    // Get the current keyboard state.
-    SDL_PumpEvents();
-    uint8_t *keystate = SDL_GetKeyState(0);
+  // Create a + shaped player, drawing each of the 5 pixels.
+  setPixel(pixels, pitch, (int)player->x, (int)player->y, 0xFFFFFFFF);
+  setPixel(pixels, pitch, (int)player->x + 1, (int)player->y, 0xFFFFFFFF);
+  setPixel(pixels, pitch, (int)player->x - 1, (int)player->y, 0xFFFFFFFF);
+  setPixel(pixels, pitch, (int)player->x, (int)player->y + 1, 0xFFFFFFFF);
+  setPixel(pixels, pitch, (int)player->x, (int)player->y - 1, 0xFFFFFFFF);
 
-    // control
-    if(keystate[SDLK_RIGHT]) {
-        player->vx += 0.2;
-    }
-    if(keystate[SDLK_LEFT]) {
-        player->vx -= 0.2;
-    }
-
-    // constant wall y-coordinate
-    const int wally = 120;
-
-    // if the player is below the floor.
-    // (y gets bigger going down)
-    if (player->y >= wally) {
-        player->y = wally;
-        player->vy *= -0.5;
-        player->vx *= 0.97;
-
-        // handle jump
-        if(keystate[SDLK_SPACE]) {
-            player->vy = -2;
-        }
-    } else {
-        // apply gravity by speeding up the player.
-        player->vy += 0.1;
-    }
-
-    // bounce off left wall.
-    if (player->x < 0) {
-        player->x = 1;
-        player->vx = -player->vx;
-    }
-
-    // bounce off right wall.
-    if (player->x > 320) {
-        player->x = 319;
-        player->vx = -player->vx;
-    }
-
-    // Apply velocity each timestep.
-    player->x += player->vx;
-    player->y += player->vy;
-
-    // wacky stuff to draw player trail.
-    // Fade the whole screen over time.
-    int i, j;
-    for(i = 0; i < screen->h; i++) {
-        for(j = 0; j < screen->w; j++) {
-            int p = getPixel(screen, j, i);
-            p &= 0x7fffffff;
-            p >>= 1;
-            setPixel(screen, j, i, p);
-        }
-    }
-
-    // Create a + shaped player, drawing each of the 5 pixels.
-    setPixel(screen, (int) player->x, (int) player->y, 0xFFFFFFFF);
-    setPixel(screen, (int) player->x+1, (int) player->y, 0xFFFFFFFF);
-    setPixel(screen, (int) player->x-1, (int) player->y, 0xFFFFFFFF);
-    setPixel(screen, (int) player->x, (int) player->y+1, 0xFFFFFFFF);
-    setPixel(screen, (int) player->x, (int) player->y-1, 0xFFFFFFFF);
+  SDL_UnlockTexture(texture);
 }
 
 // main function. Everything starts here.
-int main (int unused_argc, char **unused_argv)
-{
-    // create an SDL screen. This initializes the window.
-    screen = SDL_SetVideoMode(320, 240, 0, SDL_SWSURFACE);
+int main(int unused_argc, char **unused_argv) {
+  // Setup the SDL library.
+  SDL_Init(SDL_INIT_VIDEO);
 
-    // set a title for the window.
-    SDL_WM_SetCaption("Jumpy", 0);
+  // create an SDL screen. This initializes the window.
+  SDL_Window *window = SDL_CreateWindow(
+          "Jumpy",
+          SDL_WINDOWPOS_UNDEFINED,
+          SDL_WINDOWPOS_UNDEFINED,
+          320, 240,
+          0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Texture *texture = SDL_CreateTexture(renderer,
+                                           SDL_PIXELFORMAT_RGBA8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           320, 240);
 
-    // clear all of the pixels on the screen.
-    // (set the memory to all 0's. (black))
-    memset(screen->pixels, 0, screen->pitch * screen->h);
+  // create a new player.
+  Player player = create_player();
 
-    // create a new player.
-    Player player = create_player();
+  // track time between frames.
+  int start_tick, end_tick, delta_tick;
 
-    // track time between frames.
-    int start_tick, end_tick, delta_tick;
+  // main game loop.
+  while (running) {
+    // Start time of current frame.
+    start_tick = SDL_GetTicks();
+    // update everything.
+    handle_input(&player);
+    update(&player);
+    draw(texture, &player);
 
-    // main game loop.
-    while(running)
-    {
-        // Start time of current frame.
-        start_tick = SDL_GetTicks();
-        // update everything.
-        update(&player);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
 
-        // Delay a bit so we don't go a million frame per second.
-        end_tick = SDL_GetTicks();
-        delta_tick = end_tick - start_tick;
-        if(delta_tick < 16){
-            // Wait up to 16ms per frame. Aiming for 60fps.
-            SDL_Delay(16 - delta_tick);
-        }
+    // Draw 'renderer' context to the screen.
+    SDL_RenderPresent(renderer);
 
-        // screen is 'double buffered'.
-        // We need to call this so drawing works.
-        SDL_Flip(screen);
+    // Delay a bit so we don't go a million frame per second.
+    end_tick = SDL_GetTicks();
+    delta_tick = end_tick - start_tick;
+    if (delta_tick < 16) {
+      // Wait up to 16ms per frame. Aiming for 60fps.
+      SDL_Delay(16 - delta_tick);
     }
+  }
 
-    return 0;
+  SDL_DestroyWindow(window);
+
+  return 0;
 }
